e0920079e5666b268e0cb5a674203c80bb0d6d7b
hahglmht123 <103878643+hahglmht123@users.noreply.github.com>
2022-04-21 21:37:06 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+new WOW().init();
+var typed = new Typed('.element', {
+    strings: ["Graphic design.", "Code Developer.","Apps Designer."],
+typeSpeed:100,
+    backSpeed:100,
+    loop:true,
+  });
